5f216f9f3dcf3ff91005b9a20df24b77bc83e260
juniorpgm34 <pitagorasmattos@gmail.com>
2023-04-17 18:10:23 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
-const maxRecords = 151;
+const maxRecords = 386;
 const limit = 10;
 let offset = 0;
 
@@ -56,7 +56,7 @@ const searchInput = document.querySelector('#search');
 
 searchInput.addEventListener('input', () => {
   const searchTerm = searchInput.value;
-
+    
   if (searchTerm === '') {
     // Se o campo de pesquisa estiver vazio, mostrar todos os pokemons novamente
     pokemonList.innerHTML = '';
